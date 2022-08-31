9b98cdf12815cbbf7f2987ea7d0d169c403b2088
kbaginski97 <kamilbaginski1997@gmail.com>
2022-08-31 12:23:17 +0200

    Upadte1

--- StronaPortfolio/main.js
+++ StronaPortfolio/main.js
@@ -2,9 +2,16 @@ let hamburgerClosed = document.getElementsByClassName('hamburger-lines')[0];
 
 let navBarClosed = document.getElementsByClassName('main-header')[0];
 
+let myLink = document.querySelectorAll('.menu-item');
 
 hamburgerClosed.addEventListener('click', function () {
     hamburgerClosed.classList.toggle('hamburgerOpen');
     navBarClosed.classList.toggle('menu-opened');
 });
 
+myLink.forEach(link => {
+    link.addEventListener('click', () => {
+        navBarClosed.classList.remove('menu-opened');
+        hamburgerClosed.classList.remove('hamburgerOpen');
+    })
+})
