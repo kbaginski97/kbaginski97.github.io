60032d6dba6133ec8ed73ae7490f5547f1c0bdf7
kbaginski97 <111848818+kbaginski97@users.noreply.github.com>
2022-09-07 18:06:32 +0200

    Delete StronaSalonu directory

--- StronaSalonu/main.js
+++
@@ -1,7 +0,0 @@
-// let imageZoom = document.querySelectorAll(".zoom-out");
-
-// imageZoom.forEach(element => {
-//     element.addEventListener('click', function () {
-//         element.classList.toggle("zoom-in");
-//     })
-// })
