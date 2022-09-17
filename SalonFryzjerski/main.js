1dfecaeac5bb7c50b66cd43255de20c04946da09
kbaginski97 <kamilbaginski1997@gmail.com>
2022-09-17 19:29:27 +0200

    upd2

--- SalonFryzjerski/main.js
+++ SalonFryzjerski/main.js
@@ -1,7 +1,19 @@
-// let imageZoom = document.querySelectorAll(".zoom-out");
+const images = ["/images/Nashi.jpg", "/images/alfaparf.jpg", "/images/schwarzkopf.png"];
 
-// imageZoom.forEach(element => {
-//     element.addEventListener('click', function () {
-//         element.classList.toggle("zoom-in");
-//     })
-// })
+const carousel = document.querySelector(".cosmetics-carousel");
+const interval = setInterval(function () {
+    startCarousel();
+}, 3500);
+
+let index = 1;
+
+
+startCarousel = () => {
+
+    carousel.style.backgroundImage = `url(${images[index++]})`;
+    carousel.classList.remove("fade");
+    void carousel.offsetWidth;
+    carousel.classList.add("fade");
+
+    if (index > images.length - 1) index = 0;
+}
