f5c590a951fd51260b5e0c839013cb834b91712d
kbaginski97 <kamilbaginski1997@gmail.com>
2022-09-26 01:27:48 +0200

    bmw-landind-page

---
+++ bmwPage/js/main.js
@@ -0,0 +1,36 @@
+let hamb = document.getElementById('hamburger-closed');
+let hamburgerClosed = document.getElementsByClassName('hamburger-lines')[0];
+let navBarClosed = document.getElementsByClassName('main-header')[0];
+let myLink = document.querySelectorAll('.menu-item');
+let mainHeader = document.getElementsByClassName('main-header')[0]
+let closeFormBtn = document.getElementsByClassName('closeform')[0];
+let SwiperImg = document.getElementsByClassName('swiper-menu')[0];
+let body = document.querySelector('body');
+
+
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        type: 'bullets',
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + "</span>";
+        },
+    },
+});
+
+
+hamburgerClosed.addEventListener('click', function () {
+    hamburgerClosed.classList.toggle('hamburgerOpen');
+    navBarClosed.classList.toggle('menu-opened');
+    SwiperImg.classList.toggle('imgOpen');
+    body.classList.toggle('overflowedbody');
+});
+
+myLink.forEach(link => {
+    link.addEventListener('click', () => {
+        navBarClosed.classList.remove('menu-opened');
+        hamburgerClosed.classList.remove('hamburgerOpen');
+        body.classList.toggle('overflowedbody');
+    })
+})
