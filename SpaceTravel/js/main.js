390af158bc8b5847349dfb802aeeb64e148693c6
kbaginski97 <kamilbaginski1997@gmail.com>
2022-10-08 21:54:39 +0200

    space-travel-page

--- SpaceTravel/js/main.js
+++ SpaceTravel/js/main.js
@@ -3,8 +3,14 @@ let mobileNav = document.getElementsByClassName('main-nav')[0];
 let header = document.querySelector('header');
 let body = document.querySelector('body');
 
+mainBtn = document.getElementsByClassName('main-button')[0];
+
 mobileHamburger.addEventListener('click', function () {
   header.classList.toggle('opened-menu');
   mobileNav.classList.toggle('opened-nav');
   body.classList.toggle('body-hidden');
+})
+
+mainBtn.addEventListener('click', function () {
+  location.href = "destination.html";
 })
