f5c22f2bb67685b07f9bf95cf0f4cad1567b0fd7
kbaginski97 <kamilbaginski1997@gmail.com>
2022-11-16 12:00:17 +0100

    strona-salonu-fryzjerskieg-v1

--- StronaSalonuFryzjerskiego/main.js
+++ StronaSalonuFryzjerskiego/main.js
@@ -1,26 +1,39 @@
 hamburgerDiv = document.getElementsByClassName('hamburger-menu')[0];
 header = document.getElementsByClassName('main-header')[0];
+body = document.querySelector('body');
+MobileMenuItem = document.querySelectorAll('.mobile-item');
 
-hamburgerDiv.addEventListener('click', function () {
-    hamburgerDiv.classList.toggle('open');
-    header.classList.toggle('open');
+
+hamburgerDiv.addEventListener('click', () => {
+  hamburgerDiv.classList.toggle('open');
+  header.classList.add('open');
+  body.classList.toggle('overflow-body');
 })
 
-var swiper = new Swiper(".swiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 3500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+MobileMenuItem.forEach(element => {
+  element.addEventListener('click', () => {
+    hamburgerDiv.classList.remove('open');
+    header.classList.remove('open');
+    body.classList.toggle('overflow-body');
+  })
+});
 
-  AOS.init();
+
+var swiper = new Swiper(".swiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+AOS.init();
